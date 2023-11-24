e4441dc41840c190db1d47cadb8d4ce8b532b986
jardinerolazarte <jardineriageneral2021@gmail.com>
2023-11-24 00:02:30 -0300

    Actualizacion c 23/11

--- js/skills.js
+++ js/skills.js
@@ -4,52 +4,52 @@ const skills = [
     {
         id: 1,
         nombre: "HTML5",
-        img: "../img/html.png"
+        img: "img/html.png"
     },
     {
         id: 2,
         nombre: "CSS",
-        img: "../img/css3.png"
+        img: "img/css3.png"
     },
     {
         id: 3,
         nombre: "Bootstrap",
-        img: "../img/bootstrap.png"
+        img: "img/bootstrap.png"
     },
     {
         id: 4,
         nombre: "Javascript",
-        img: "../img/js.png"
+        img: "img/js.png"
     },
     {
         id: 5,
         nombre: "Git",
-        img: "../img/git.png"
+        img: "img/git.png"
     },
     {
         id: 6,
         nombre: "GitHub",
-        img: "../img/GitHub-Mark.png"
+        img: "img/GitHub-Mark.png"
     },
     {
         id: 7,
         nombre: "Lenguaje C",
-        img: "../img/lenguajec.png"
+        img: "img/lenguajec.png"
     },
     {
         id: 8,
         nombre: "MySQL",
-        img: "../img/basededatos.png"
+        img: "img/basededatos.png"
     },
     {
         id: 9,
         nombre: "Java",
-        img: "../img/java.jpg"
+        img: "img/java.jpg"
     },
     {
         id: 10,
         nombre: "PHP",
-        img: "../img/php.png"
+        img: "img/php.png"
     },
     {
         id: 11,
